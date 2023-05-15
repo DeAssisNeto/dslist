a05c7646531cb08427eddd08d1550c477e905efb
Francisco de Assis <francisco.pereira1713@gmail.com>
2023-05-14 23:31:46 -0300

    Consulta SQL, projection, games by list

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dslist.dto;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
     private Long id;
@@ -13,12 +14,21 @@ public class GameMinDTO {
 
     }
 
+    public GameMinDTO(GameMinProjection projection) {
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
+    }
+
+
     public GameMinDTO(Game gameEntity) {
         id = gameEntity.getId();
         title = gameEntity.getTitle();
         year = gameEntity.getYear();
         imgUrl = gameEntity.getImgUrl();
-        shortDescription = gameEntity.getLongDescription();
+        shortDescription = gameEntity.getShorDescription();
     }
 
     public Long getId() {
